0a69baf706f43ba55e0133001a8a6eeccd6ed1f6
Nawalnazari <nawalnazari4@gmail.com>
2025-08-10 14:31:03 +0800

    set base for vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   define: {
-    'process.env': process.env
+    "process.env": process.env
   },
   plugins: [react()],
+  base: "/ZapinFestWeb/",
   build: {
     commonjsOptions: {
-      transformMixedEsModules: true,
+      transformMixedEsModules: true
     }
-  } 
-})
+  }
+});
