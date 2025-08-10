b11a8665dd6056e60f054b0eafa8bbfc3cf6f7e0
Nawalnazari <nawalnazari4@gmail.com>
2025-08-11 01:07:38 +0800

    fix: enable balik header

--- src/pages/SiaranLangsung.jsx
+++ src/pages/SiaranLangsung.jsx
@@ -65,7 +65,7 @@ function SiaranLangsung() {
       {/* Content area */}
       <div className="relative flex flex-col flex-1 overflow-y-auto overflow-x-hidden">
         {/*  Site header */}
-        {/* <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} /> */}
+        <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
 
         <main className="grow">
           <div className="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-4xl mx-auto">
